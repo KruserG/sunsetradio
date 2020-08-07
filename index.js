8c034d501611dc5a28e5d836ce26772676589f3a
Jason Kruser <yokariig@gmail.com>
2020-08-07 08:08:23 -0400

    fixing fix command

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ try{
     await connection.play(broadcast);
 }catch(e){
     message.react("❌")
-    message.reply("échec de réparation, merci de réessayer.")
+    message.reply("une erreur est survenue lors de la tentative de traitement de votre réparation, merci de réessayer.")
 }
       
 
